ce99b102360d1754acf5c7e76b3715cb8bc886e0
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-11-07 18:42:39 +0300

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,14 +4,17 @@ const { InteractionType } = require("discord.js")
 
 module.exports = {
 	name: 'interactionCreate',
-	execute(interaction, client) {
-	  if (!interaction.type === InteractionType.ApplicationCommand) {
+	async execute(interaction, client, dbl) {
+	  if (interaction.type !== InteractionType.ApplicationCommand) {
 		  //butona basıldığında burda hangi işlemleri yapacağınızı belirleyebilirsiniz.
 	  } else {
 		const command = client.slashcommands.get(interaction.commandName);
 		if (!command) return;
 		try {
-		  command.execute(client, interaction);
+			await dbl.getVotes().then(x => { 
+				if(command.dbl && !x.filter(y => y.id === interaction.user.id).length) return interaction.reply("Bu komutu kullanmak için bota oy vermeniz gerekiyor.")
+				else command.execute(client, interaction, dbl);
+			})
 		} catch (error) {
 		  console.error(error);
 		  interaction.reply({ content: 'Komutta bir sorun oluştu lütfen daha sonra tekrar dene 😔', ephemeral: true });
