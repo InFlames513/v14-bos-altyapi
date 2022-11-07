ad1cb312f23f739b7c3bee0314fc440e63f4e3f0
InFlames <85372327+InFlames513@users.noreply.github.com>
2022-11-07 18:45:47 +0300

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,15 +1,23 @@
 //burasının ne olduğunu bilmiyorsanız lütfen hiç bir koda dokunmayın.
 
-const { prefix } = require("../base/settings.json");
+const { prefix, topgg } = require("../base/settings.json");
+
 module.exports = {
   name: "messageCreate",
-  async execute(message, client) {
+  async execute(message, client, dbl) {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     let command = message.content.split(" ")[0].slice(prefix.length);
     let args = message.content.split(" ").slice(1);
     let cmd = client.commands.get(command)
     if (!cmd) return;
-      cmd.execute(client, message, args);
+    if(topgg) {
+      await dbl.getVotes().then(x => { 
+        if(cmd.dbl && !x.filter(y => y.id === message.author.id).length) return message.reply("Bu komutu kullanmak için bota oy vermeniz gerekiyor.")
+        else cmd.execute(client, message, args, dbl);
+      })
+    } else {
+      cmd.execute(client, message, args, dbl);
+    }
   },
 };
